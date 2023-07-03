e4bf6423bffc44cf96e67214326af7789067ad75
Nathan Wick <nathanwick2001@gmail.com>
2023-07-03 17:15:44 -0400

    Update Window Context

--- nathan-wick/src/contexts/Window.tsx
+++ nathan-wick/src/contexts/Window.tsx
@@ -2,37 +2,96 @@ import React, {FC, createContext, useEffect, useState,} from "react";
 
 export const WindowContext = createContext<{
         scroll: number,
+        size: `large` | `medium` | `small`
     }>({
         "scroll": 0,
+        "size": `small`,
     },),
     WindowContextProvider: FC<{
         children: JSX.Element
     }> = ({children,},) => {
 
-        const [
-                scroll,
-                setScroll,
-            ] = useState(0,),
-            onScroll = () => {
+        const getScroll = () => {
 
                 const Scrolled = document.documentElement.scrollTop,
                     MaxHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight,
                     ScrollPercent = Scrolled / MaxHeight * 100;
-                setScroll(ScrollPercent,);
+                return ScrollPercent;
 
-            };
+            },
+            getSize = () => {
 
-        useEffect(() => {
+                const {width,} = window.screen;
+                if (width < 768) {
 
-            window.addEventListener(
-                `scroll`,
-                onScroll,
-            );
+                    return `small`;
 
-        },);
+                } else if (width < 992) {
+
+                    return `medium`;
+
+                }
+
+                return `large`;
+
+
+            },
+            [
+                scroll,
+                setScroll,
+            ] = useState(getScroll(),),
+            [
+                size,
+                setSize,
+            ] = useState<`large` | `medium` | `small`>(getSize(),);
+
+        useEffect(
+            () => {
+
+                window.addEventListener(
+                    `scroll`,
+                    () => setScroll(getScroll(),),
+                );
+
+                return () => {
+
+                    window.removeEventListener(
+                        `scroll`,
+                        () => setScroll(getScroll(),),
+                    );
+
+                };
+
+            },
+            [],
+        );
+
+        useEffect(
+            () => {
+
+                window.addEventListener(
+                    `resize`,
+                    () => setSize(getSize(),),
+                );
+
+                return () => {
+
+                    window.removeEventListener(
+                        `resize`,
+                        () => setSize(getSize(),),
+                    );
+
+                };
+
+            },
+            [],
+        );
 
         return <WindowContext.Provider
-            value={{scroll,}}>
+            value={{
+                scroll,
+                size,
+            }}>
             {children}
         </WindowContext.Provider>;
 
