64c87566f2153c3592d3f457c150515f2353aca6
Nathan Wick <nathanwick2001@gmail.com>
2023-07-20 18:56:10 -0400

    Updates

--- scripts/url-path-truncation-and-encoding.js
+++ scripts/url-path-truncation-and-encoding.js
@@ -1,28 +1,9 @@
-/* eslint-disable require-unicode-regexp */
-/* eslint-disable id-length */
-
-const pathSegmentsToKeep = 0,
-    l = window.location;
-
-l.replace(`${l.protocol}//${l.hostname}${l.port
-    ? `:${l.port}`
-    : ``
-}${l.pathname.split(`/`,).slice(
-    0,
-    1 + pathSegmentsToKeep,
-).
-    join(`/`,)}/?/${
-    l.pathname.slice(1,).split(`/`,).
-        slice(pathSegmentsToKeep,).
-        join(`/`,).
-        replace(
-            /&/g,
-            `~and~`,
-        )
-}${l.search
-    ? `&${l.search.slice(1,).replace(
-        /&/g,
-        `~and~`,
-    )}`
-    : ``
-}${l.hash}`,);
+var pathSegmentsToKeep = 0;
+var l = window.location;
+l.replace(
+    l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') +
+    l.pathname.split('/').slice(0, 1 + pathSegmentsToKeep).join('/') + '/?/' +
+    l.pathname.slice(1).split('/').slice(pathSegmentsToKeep).join('/').replace(/&/g, '~and~') +
+    (l.search ? '&' + l.search.slice(1).replace(/&/g, '~and~') : '') +
+    l.hash
+);
--- scripts/url-query-param-decoder.js
+++ scripts/url-query-param-decoder.js
@@ -1,26 +1,10 @@
-/* eslint-disable require-unicode-regexp */
-/* eslint-disable id-length */
-/* eslint-disable func-names */
-
-(function (l,) {
-
-    if (l.search[1] === `/`) {
-
-        const decoded = l.search.slice(1,).split(`&`,).
-            map((s,) => s.replace(
-                /~and~/g,
-                `&`,
-            ),).
-            join(`?`,);
-        window.history.replaceState(
-            null,
-            null,
-            l.pathname.slice(
-                0,
-                -1,
-            ) + decoded + l.hash,
-        );
-
+(function(l) {
+    if (l.search[1] === '/' ) {
+      var decoded = l.search.slice(1).split('&').map(function(s) { 
+        return s.replace(/~and~/g, '&')
+      }).join('?');
+      window.history.replaceState(null, null,
+          l.pathname.slice(0, -1) + decoded + l.hash
+      );
     }
-
-}(window.location,));
+  }(window.location))
